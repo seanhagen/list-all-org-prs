88b7ffbec0f04fa9689a11c802cf532d792c4c44
Sean Patrick Hagen <sean.hagen@gmail.com>
2016-08-12 19:35:29 -0700

    Setting up the server

---
+++ server/server.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+type AuthType int
+
+const (
+	AUTH_NONE  AuthType = 1
+	AUTH_TOKEN AuthType = 2
+)
+
+type (
+	HandleFunc func() httprouter.Handle
+
+	Route struct {
+		AuthType AuthType
+		Handler  HandleFunc
+	}
+
+	RouteMap map[string]map[string]Route
+)
+
+// CreateRoute does a thing
+func CreateRoute(auth AuthType, handler HandleFunc) Route {
+	if auth != AUTH_TOKEN {
+		auth = AUTH_NONE
+	}
+
+	return Route{
+		AuthType: auth,
+		Handler:  handler,
+	}
+}
+
+func GetEmptyRoutes() RouteMap {
+	routes := make(RouteMap)
+	verbs := []string{"GET", "PUT", "POST", "OPTIONS", "PATCH", "DELETE"}
+	for _, v := range verbs {
+		routes[v] = make(map[string]Route)
+	}
+	return routes
+}
